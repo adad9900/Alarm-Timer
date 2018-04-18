de1755ca599c650d8d1538e97ae810c5d7e305a7
D7703_16 <d7703_16@D7703-16ui-iMac.local>
2018-04-18 12:34:08 +0900

    background color change

--- Alarm Timer/ViewController.swift
+++ Alarm Timer/ViewController.swift
@@ -10,16 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myDatePicker: UIDatePicker!
+    @IBOutlet weak var alarmTime: UILabel!
+    @IBOutlet weak var timeNow: UILabel!
+    var myTimer = Timer()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm:ss"
+        alarmTime.text = formatter.string(from: myDatePicker.date)
+        
+        myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in self.updateTime()
+            
+        })
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func updateTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm:ss"
+        timeNow.text = formatter.string(from: date)
+        
+        if timeNow.text == alarmTime.text {
+            view.backgroundColor = UIColor.yellow // 지정한 알람 시간이되면 노란색으로
+        }
     }
-
-
+    
+    
+    @IBAction func DatePickerValueChanged(_ sender: Any) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm:ss"
+        alarmTime.text = formatter.string(from : myDatePicker.date)
+    }
+    
+    @IBAction func button(_ sender: Any) {
+        view.backgroundColor = UIColor.white
+    }
 }
+
+
+
 
